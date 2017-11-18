59177514efd2978da3aa99bef181b67607272a27
Tom Milligan <tom@tommilligan.net>
2017-11-18 10:32:44 +0000

    Working empty es6-mocha-flow project

--- test/test_index.js
+++ test/test_index.js
@@ -1,14 +1,12 @@
 var chai = require('chai');
-var shoppingBasket = require('../src/index.js');
+var subject = require('../src/index.js');
 
 var expect = chai.expect;
 
 describe('index.js', function(){
     describe('pass', function(){
-        describe('construction', function(){
-            it('should provide default arguments', function(){
-                expect(null).to.be.null;
-            });
+        it('should be null', function(){
+            expect(subject).to.be.null;
         });
     });
 });
