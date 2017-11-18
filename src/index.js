59795481d24c4852940c006042124e71232957c2
Tom Milligan <tom@tommilligan.net>
2017-11-18 15:21:57 +0000

    Most shop + basket functionality in place

--- src/index.js
+++ src/index.js
@@ -1,9 +1,122 @@
 // @flow
 
-class Foo {
-    bar = () => {
-        return "bar";
+class BasketError extends Error {}
+class ShopError extends Error {}
+
+type Id = string;
+
+interface Item {
+    id: Id;
+    price: number;
+    title: string;
+}
+
+class Shop {
+    items: Map<Id, Item>;
+
+    constructor(items?: Array<Item>) {
+        this.items = new Map();
+        if (items) {
+            items.forEach(item => {
+                this.add(item);
+            });
+        }
+    }
+
+    add = (item: Item, overwrite: boolean = false): void => {
+        let id = item.id;
+        if (!this.contains(id) || overwrite) {
+            this.items.set(id, item);
+        } else {
+            throw new ShopError(`Item '${id}' is already in shop`);
+        }
+    }
+
+    contains = (id: Id): boolean => {
+        return this.items.has(id);
+    }
+
+    get = (id: Id): Item => {
+        let item = this.items.get(id);
+        if (item) {
+            return item;
+        } else {
+            throw new ShopError(`Item '${id}' is not in shop`);
+        }
     }
 }
 
-module.exports = Foo;
+class Basket {
+    shop: Shop;
+    contents: Map<Id, number>
+
+    constructor(shop: Shop) {
+        this.shop = shop;
+        this.contents = new Map();
+    }
+
+    add = (id: Id): void => {
+        if (!this.contains(id)) {
+            this.contents.set(id, 1);
+        } else {
+            throw new BasketError(`Item '${id}' already in basket`);
+        }
+    }
+
+    adjustQuantity = (id: Id, adjustment: number): void => {
+        let quantity = this.getQuantity(id);
+        this.setQuantity(id, quantity + adjustment);
+    }
+
+    contains = (id: Id): boolean => {
+        return this.contents.has(id);
+    }
+
+    decrement = (id: Id): void => {
+        this.adjustQuantity(id, -1);
+    }
+
+    get = (id: Id): Item => {
+        return this.shop.get(id);
+    }
+
+    getQuantity = (id: Id): number => {
+        let quantity = this.contents.get(id);
+        if (quantity) {
+            return quantity;
+        } else {
+            throw new BasketError(`Item '${id}' is not in basket`);
+        }
+    }
+
+    increment = (id: Id): void => {
+        this.adjustQuantity(id, +1);
+    }
+
+    remove = (id: Id): void => {
+        if (this.contains(id)) {
+            this.contents.delete(id);
+        } else {
+            throw new BasketError(`Item '${id}' is not in basket`);
+        }
+    }
+
+    setQuantity = (id: Id, quantity: number): void => {
+        this.contents.set(id, quantity);        
+    }
+
+    total = (): number => {
+        let accumulator: number = 0.0;
+        for (let [id, quantity] of this.contents) {
+            accumulator += (quantity * this.shop.get(id).price);
+        }
+        return accumulator;
+    }
+}
+
+module.exports = {
+    Basket,
+    Shop,
+    BasketError,
+    ShopError
+};
--- test/test_index.js
+++ test/test_index.js
@@ -1,12 +1,87 @@
-var chai = require("chai");
-var subject = require("../src/index.js");
+// @flow
+import { describe, it, beforeEach } from "mocha";
+import chai from "chai";
+import { Basket, Shop } from "../src/index.js";
 
-var expect = chai.expect;
+let expect = chai.expect;
 
-describe("index.js", function(){
-    describe("class", function(){
-        it("should support arrow method syntax", function(){
-            expect(new subject().bar()).to.equal("bar");
+let milk = {
+    id: "6318dbff-8e44-48df-97a5-c3430d2c8861",
+    price: 1.49,
+    title: "Milk (2L)",
+};
+let bread = {
+    id: "667fa050-2249-462b-b3db-f75bc51ec7f3",
+    price: 0.90,
+    title: "Bread (Wholemeal)",
+};
+
+describe("index.js", function() {
+    describe("shop construction", function() {
+        let shop = new Shop();
+        beforeEach(function() {
+            shop = new Shop();
+        });
+        it("should store and retrieve item", function() {
+            shop.add(milk);
+            let actual = shop.get(milk.id);
+            expect(actual).to.equal(milk);
+        });
+        it("should store an array of items during construction", function() {
+            shop = new Shop([milk, bread]);
+            let actual = shop.get(bread.id);
+            expect(actual).to.equal(bread);
+        });
+    });
+    describe("working shop", function() {
+        let shop = new Shop();
+        beforeEach(function() {
+            shop = new Shop([milk]);
+        });
+        it("should not overwrite items by default", function() {
+            expect(function() {
+                shop.add(milk);
+            }).to.throw;
+        });
+        it("should overwrite items if requested", function() {
+            let milkOrganic = {
+                price: 2.49,
+                ...milk
+            };
+            shop.add(milkOrganic, true);
+            expect(shop.get(milk.id).price).to.equal(milkOrganic.price);            
+        });
+        it("should throw error for non-existant items", function() {
+            expect(function() {
+                shop.get("spam");
+            }).to.throw;
+        });
+        describe("basket", function() {
+            let basket = new Basket(shop);
+            beforeEach(function() {
+                basket = new Basket(shop);
+            });
+            it("add item from shop", function() {
+                basket.add(milk.id);
+                let quantity = basket.quantity(milk.id);
+                expect(quantity).to.equal(1);
+            });
+            it("add and remove item from shop", function() {
+                basket.add(milk.id);
+                basket.remove(milk.id);
+                expect(basket.contains(milk.id)).to.be.false;
+            });
+            it("should throw error for remoing non-existant item", function() {
+                expect(function () {
+                    basket.remove(milk.id);
+                }).to.throw;
+            });
+            it("should calculate total correctly", function() {
+                basket.add(milk.id);
+                shop.add(bread);
+                basket.add(bread.id);
+                expect(basket.total()).to.equal(2.39);
+            });
         });
     });
 });
